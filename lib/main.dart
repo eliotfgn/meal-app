b8a19626f9dab5092d87fdad9c6932ab979e1139
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-04 21:02:02 +0100

    add main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Meal App"),
+        ),
+      ),
+    );
+  }
+}
