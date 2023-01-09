47b216881358aa2b31e08e65111af4acb639681a
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-09 01:06:43 +0100

    refactor enum names

--- lib/data.dart
+++ lib/data.dart
@@ -64,10 +64,10 @@ const DUMMY_MEALS = [
       'c2',
     ],
     title: 'Spaghetti with Tomato Sauce',
-    affordability: Affordability.affordable,
-    complexity: Complexity.simple,
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
     imageUrl:
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg/800px-Spaghetti_Bolognese_mit_Parmesan_oder_Grana_Padano.jpg',
     duration: 20,
     ingredients: [
       '4 Tomatoes',
@@ -97,10 +97,10 @@ const DUMMY_MEALS = [
       'c2',
     ],
     title: 'Toast Hawaii',
-    affordability: Affordability.affordable,
-    complexity: Complexity.simple,
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
     imageUrl:
-    'https://cdn.pixabay.com/photo/2018/07/11/21/51/toast-3532016_1280.jpg',
+        'https://cdn.pixabay.com/photo/2018/07/11/21/51/toast-3532016_1280.jpg',
     duration: 10,
     ingredients: [
       '1 Slice White Bread',
@@ -126,10 +126,10 @@ const DUMMY_MEALS = [
       'c3',
     ],
     title: 'Classic Hamburger',
-    affordability: Affordability.pricey,
-    complexity: Complexity.simple,
+    affordability: Affordability.Pricey,
+    complexity: Complexity.Simple,
     imageUrl:
-    'https://cdn.pixabay.com/photo/2014/10/23/18/05/burger-500054_1280.jpg',
+        'https://cdn.pixabay.com/photo/2014/10/23/18/05/burger-500054_1280.jpg',
     duration: 45,
     ingredients: [
       '300g Cattle Hack',
@@ -157,10 +157,10 @@ const DUMMY_MEALS = [
       'c4',
     ],
     title: 'Wiener Schnitzel',
-    affordability: Affordability.luxurious,
-    complexity: Complexity.challenging,
+    affordability: Affordability.Luxurious,
+    complexity: Complexity.Challenging,
     imageUrl:
-    'https://cdn.pixabay.com/photo/2018/03/31/19/29/schnitzel-3279045_1280.jpg',
+        'https://cdn.pixabay.com/photo/2018/03/31/19/29/schnitzel-3279045_1280.jpg',
     duration: 60,
     ingredients: [
       '8 Veal Cutlets',
@@ -194,10 +194,10 @@ const DUMMY_MEALS = [
       'c10',
     ],
     title: 'Salad with Smoked Salmon',
-    affordability: Affordability.luxurious,
-    complexity: Complexity.simple,
+    affordability: Affordability.Luxurious,
+    complexity: Complexity.Simple,
     imageUrl:
-    'https://cdn.pixabay.com/photo/2016/10/25/13/29/smoked-salmon-salad-1768890_1280.jpg',
+        'https://cdn.pixabay.com/photo/2016/10/25/13/29/smoked-salmon-salad-1768890_1280.jpg',
     duration: 15,
     ingredients: [
       'Arugula',
@@ -229,10 +229,10 @@ const DUMMY_MEALS = [
       'c10',
     ],
     title: 'Delicious Orange Mousse',
-    affordability: Affordability.affordable,
-    complexity: Complexity.hard,
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Hard,
     imageUrl:
-    'https://cdn.pixabay.com/photo/2017/05/01/05/18/pastry-2274750_1280.jpg',
+        'https://cdn.pixabay.com/photo/2017/05/01/05/18/pastry-2274750_1280.jpg',
     duration: 240,
     ingredients: [
       '4 Sheets of Gelatine',
@@ -264,10 +264,10 @@ const DUMMY_MEALS = [
       'c7',
     ],
     title: 'Pancakes',
-    affordability: Affordability.affordable,
-    complexity: Complexity.simple,
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Simple,
     imageUrl:
-    'https://cdn.pixabay.com/photo/2018/07/10/21/23/pancake-3529653_1280.jpg',
+        'https://cdn.pixabay.com/photo/2018/07/10/21/23/pancake-3529653_1280.jpg',
     duration: 20,
     ingredients: [
       '1 1/2 Cups all-purpose Flour',
@@ -295,10 +295,10 @@ const DUMMY_MEALS = [
       'c8',
     ],
     title: 'Creamy Indian Chicken Curry',
-    affordability: Affordability.pricey,
-    complexity: Complexity.challenging,
+    affordability: Affordability.Pricey,
+    complexity: Complexity.Challenging,
     imageUrl:
-    'https://cdn.pixabay.com/photo/2018/06/18/16/05/indian-food-3482749_1280.jpg',
+        'https://cdn.pixabay.com/photo/2018/06/18/16/05/indian-food-3482749_1280.jpg',
     duration: 35,
     ingredients: [
       '4 Chicken Breasts',
@@ -328,10 +328,10 @@ const DUMMY_MEALS = [
       'c9',
     ],
     title: 'Chocolate Souffle',
-    affordability: Affordability.affordable,
-    complexity: Complexity.hard,
+    affordability: Affordability.Affordable,
+    complexity: Complexity.Hard,
     imageUrl:
-    'https://cdn.pixabay.com/photo/2014/08/07/21/07/souffle-412785_1280.jpg',
+        'https://cdn.pixabay.com/photo/2014/08/07/21/07/souffle-412785_1280.jpg',
     duration: 45,
     ingredients: [
       '1 Teaspoon melted Butter',
@@ -379,10 +379,10 @@ const DUMMY_MEALS = [
       'c10',
     ],
     title: 'Asparagus Salad with Cherry Tomatoes',
-    affordability: Affordability.luxurious,
-    complexity: Complexity.simple,
+    affordability: Affordability.Luxurious,
+    complexity: Complexity.Simple,
     imageUrl:
-    'https://cdn.pixabay.com/photo/2018/04/09/18/26/asparagus-3304997_1280.jpg',
+        'https://cdn.pixabay.com/photo/2018/04/09/18/26/asparagus-3304997_1280.jpg',
     duration: 30,
     ingredients: [
       'White and Green Asparagus',
--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,14 +1,6 @@
-enum Complexity {
-  simple,
-  challenging,
-  hard
-}
+enum Complexity { Simple, Challenging, Hard }
 
-enum Affordability {
-  affordable,
-  pricey,
-  luxurious
-}
+enum Affordability { Affordable, Pricey, Luxurious }
 
 class Meal {
   final String id;
@@ -25,19 +17,18 @@ class Meal {
   final bool isVegan;
   final bool isVegetarian;
 
-  const Meal({
-    required this.id,
-    required this.categories,
-    required this.title,
-    required this.imageUrl,
-    required this.ingredients,
-    required this.steps,
-    required this.duration,
-    required this.complexity,
-    required this.affordability,
-    required this.isGlutenFree,
-    required this.isLactoseFree,
-    required this.isVegan,
-    required this.isVegetarian
-  });
+  const Meal(
+      {required this.id,
+      required this.categories,
+      required this.title,
+      required this.imageUrl,
+      required this.ingredients,
+      required this.steps,
+      required this.duration,
+      required this.complexity,
+      required this.affordability,
+      required this.isGlutenFree,
+      required this.isLactoseFree,
+      required this.isVegan,
+      required this.isVegetarian});
 }
